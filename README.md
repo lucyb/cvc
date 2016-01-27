f248df9490c6d8913e646737a4bd7b0bbc2f72aa
lucyb <lucybridges@gmail.com>
2016-01-27 18:04:46 +0000

    Mini project for generating CVC style passwords
    Restructured slightly and added a new project for generating secure
    and easy to remember passwords.

---
+++ README.md
@@ -0,0 +1,18 @@
+# CVC Password
+
+Generate a password designed for high memorability and security. 
+
+This uses a CVC-CVC-CVC (consonant-vowel-consonant) construction. It provides a list of three randomly generated passwords, so the most memorable one can be selected.
+
+It follows the advice given in the GCHQ password guidance document.
+
+## How does it work?
+
+You'll need python3, that's it. 
+
+Install using 
+
+'''python setup.py install'''
+
+Then just type 'cvc'.
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cvc-password',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click'
+    ],
+    entry_points='''
+        [console_scripts]
+        cvc=cvc.cli:run
+    ''',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        ],
+)
+
